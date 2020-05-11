ced25390573a1fac49fa936bd2ee9715608b068e
Arjun <argupta@mit.edu>
2020-05-11 12:38:19 -0400

    Added renamed timing.py

---
+++ timer.py
@@ -0,0 +1,21 @@
+from timing.time_pointlocation import DecompositionCases
+from timing.time_query import QueryCases
+from timing.time_minkowski import MinkowskiCases
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--time_trap_building", action="store_true", help="time the trapezoid and point-location building algorithm")
+parser.add_argument("--time_trap_querying", action="store_true", help="time the trapezoid and point-location query time")
+parser.add_argument("--time_minkowski", action="store_true", help="time minkowski sum")
+parser.add_argument("--time_minkowski_slow", action="store_true", help="time minkowski O(mn) sum")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    if args.time_trap_building:
+        timer = DecompositionCases()
+    elif args.time_trap_querying:
+        timer = QueryCases()
+    elif args.time_minkowski:
+        timer = MinkowskiCases()
+    timer.run()
+
